743da5bc3f734e983209ab99bfba7fe21bbea57c
Andraz Brodnik <brodul@brodul.org>
2014-01-23 10:49:26 +0100

    Move and minor changes in pre and post almir install scripts

--- roles/almir/files/pre_almir.sh
+++ roles/almir/files/pre_almir.sh
@@ -1,7 +1,8 @@
 set +xe
 PYTHON_EXEC=$(which python2.7 || which python2.6)
 [ -z $PYTHON_EXEC ] && $(which echo) "No python2.7 or python2.6 binary found, exiting" && exit 1
-git clone https://github.com/iElectric/almir.git -b latests .
+git clone https://github.com/iElectric/almir.git -b latests tmp
+cp -r tmp/* .
 printf "[buildout]\nextends = buildout.d/production.cfg" > buildout.cfg
 $PYTHON_EXEC bootstrap.py -v 1.7.1
 bin/buildout
